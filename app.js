2e9f1299c188c28487c3582881781c11f13a6c51
NichRDataBase <134532458+NichRDataBase@users.noreply.github.com>
2023-05-26 12:30:12 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const modal = document.querySelector('.modal');
-const previews = document.querySelector(".grid-item grid-item-5");
-const original = document.querySelector("full-image");
-const imgText=document.querySelector(".caption");
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".grid-container2 img");
+const original = document.querySelector(".full-image");
+const imgText = document.querySelector(".caption");
 
 previews.forEach((preview)=>{
-    previews.addEventListener("click",() =>{
+    preview.addEventListener("click", () =>{
         modal.classList.add("open");
+        original.classList.add("open");
+
+        const originalSrc= './images/XboxPFP.jpg';
     });
 });
 
